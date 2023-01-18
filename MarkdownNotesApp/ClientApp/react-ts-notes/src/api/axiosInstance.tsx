7b4d557584d269bab89c22e774ccb4e2b7ef03ad
vmarcell96 <vajdam96@gmail.com>
2023-01-18 11:14:00 +0100

    REFACTOR: renamed default export

--- MarkdownNotesApp/ClientApp/react-ts-notes/src/api/axiosInstance.tsx
+++ MarkdownNotesApp/ClientApp/react-ts-notes/src/api/axiosInstance.tsx
@@ -1,22 +1,24 @@
 import axios from 'axios';
 
 const getBaseUrl = () => {
-    let url;
-    switch(process.env.NODE_ENV) {
-      case 'production':
-        url = 'https://noteappapi.azurewebsites.net';
-        break;
-      case 'development':
-      default:
-        url = 'https://localhost:7218';
-    }
-    return url;
+  let url;
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      url = 'https://noteappapi.azurewebsites.net';
+      break;
+    case 'development':
+    default:
+      url = 'https://localhost:7218';
   }
+  return url;
+}
 
-export default axios.create({
-    baseURL: getBaseUrl(),
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
+const axiosInstance = axios.create({
+  baseURL: getBaseUrl(),
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
 });
+
+export default axiosInstance;
