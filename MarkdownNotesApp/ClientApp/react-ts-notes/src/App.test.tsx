3a21b0925c0f79d9d7a15a639e75357d1b2d3f0d
vmarcell96 <vajdam96@gmail.com>
2023-01-18 11:36:47 +0100

    TEST: Wrote first test case

---
+++ MarkdownNotesApp/ClientApp/react-ts-notes/src/App.test.tsx
@@ -0,0 +1,11 @@
+import { describe, test, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+describe('<App />', () => {
+    test('Test', () => {
+
+        expect(true).toBeTruthy()
+
+    })
+});
