9cbecbe7058fe1093e517f5f2476eb99a7fb6ae6
vmarcell96 <vajdam96@gmail.com>
2023-01-18 11:14:53 +0100

    FEAT: Added error handling to getAllNotes and getAllTags functions

--- MarkdownNotesApp/ClientApp/react-ts-notes/src/services/notes.service.ts
+++ MarkdownNotesApp/ClientApp/react-ts-notes/src/services/notes.service.ts
@@ -1,19 +1,76 @@
 import { AxiosResponse } from 'axios';
 import { Note, NoteDto } from '../@types/notes';
-import axios from '../api/axiosInstance';
+import { AxiosError } from 'axios';
+import axiosInstance from '../api/axiosInstance';
+import axios from 'axios';
+
+type GetAllNotesResponse = {
+    data: Array<Note>;
+};
+
+type CustomError = {
+    url: string,
+    method: string,
+    name: string,
+    message: string,
+}
+
 
 export function useNotesService() {
 
-    const getAllNotes = async () => {
-        const res: AxiosResponse<Array<Note> | any> = await axios.get<Array<Note>>("/api/notes");
-        if (res !== undefined) {
-            return res.data;
+    const getAllNotes = async (): Promise<Array<Note> | null> => {
+        try {
+
+            const { data, status } = await axiosInstance.get<Array<Note>>("/api/notes");
+            return data;
+
+        } catch (error) {
+
+            const err = error as Error | AxiosError;
+            if (axios.isAxiosError(err)) {
+                // axios error
+                console.log('Error at url: ' + err.config?.baseURL + err.config?.url);
+                console.log('Method: ' + err.config?.method)
+                console.log('Error name: ' + err.name)
+                console.log('Error message: ' + err.message)
+                console.log('Error response data: ' + err.response?.data)
+                console.log('Error response status: ' + err.response?.status)
+            } else if (err instanceof Error) {
+                // native error
+                console.log('unexpected error: ', err);
+            } else {
+                // unknown
+                console.log('unknown error: ', err);
+            }
+            // const customError = {
+            //     url: err.config?.baseURL + err.config?.url
+            // }
+            return null;
         }
-        return null;
     }
 
-    const getNoteById = async (id: string) => {
-        const res: AxiosResponse<Note | any> = await axios.get<Note>(`/api/notes/${id}`);
+    const getNoteById = async (id: string): Promise<Note | null> => {
+        try {
+
+            const { data, status } = await axiosInstance.get<Note>(`/api/notes/${id}`);
+            return data;
+
+        } catch (error) {
+            
+            const err = error as Error | AxiosError;
+            if (axios.isAxiosError(err)) {
+                // axios error
+                console.log('error message: ', err.message);
+            } else if (err instanceof Error) {
+                // native error
+                console.log('unexpected error: ', err);
+            } else {
+                // unknown
+                console.log('unknown error: ', err);
+            }
+            return null;
+        }
+        const res: AxiosResponse<Note | any> = await axiosInstance.get<Note>(`/api/notes/${id}`);
         if (res !== undefined) {
             return res.data;
         }
@@ -21,7 +78,7 @@ export function useNotesService() {
     }
 
     const addNewNote = async (data: NoteDto) => {
-        const res: AxiosResponse<Note | any> = await axios.post<Note>("/api/notes", data);
+        const res: AxiosResponse<Note | any> = await axiosInstance.post<Note>("/api/notes", data);
         if (res !== undefined) {
             return res.data;
         }
@@ -29,7 +86,7 @@ export function useNotesService() {
     }
 
     const updateNoteById = async (data: NoteDto, id: string) => {
-        const res: AxiosResponse<Note | any> = await axios.put<Note>(`/api/notes/${id}`, data);
+        const res: AxiosResponse<Note | any> = await axiosInstance.put<Note>(`/api/notes/${id}`, data);
         if (res !== undefined) {
             return res.data;
         }
@@ -37,7 +94,7 @@ export function useNotesService() {
     }
 
     const deleteNoteById = async (id: string) => {
-        const res: AxiosResponse<any | any> = await axios.delete<any>(`/api/notes/${id}`);
+        const res: AxiosResponse<any | any> = await axiosInstance.delete<any>(`/api/notes/${id}`);
         if (res !== undefined) {
             return res.data;
         }
