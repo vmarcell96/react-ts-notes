9e4f120c29ee3c098bac80338a0fee6593392960
vmarcell96 <vajdam96@gmail.com>
2023-01-26 11:21:25 +0100

    Implemented autosave feature in edit component, autosave happens every 5 minutes

--- MarkdownNotesApp/ClientApp/react-ts-notes/src/components/NoteForm.tsx
+++ MarkdownNotesApp/ClientApp/react-ts-notes/src/components/NoteForm.tsx
@@ -1,6 +1,6 @@
-import { FormEvent, useRef, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import { Form, Stack, Col, Row, Button } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
 import { NoteDto, Tag, TagDto } from "../@types/notes";
 
@@ -18,6 +18,30 @@ export function NoteForm({ onSubmit, availableTags, title = "", markdown = "", t
     const markdownRef = useRef<HTMLTextAreaElement>(null)
     const [selectedTags, setSelectedTags] = useState<TagDto[]>(tags)
     const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        //if we are editing a note, save it every 5 minutes
+        if (location.pathname.slice(-4) === "edit") {
+            const timerId = setTimeout(() => {
+                //if the user already wrote something
+                if (markdownRef.current!.value !== "" && titleRef.current!.value !== "") {
+                    onSubmit({
+                        // We made sure with the "required" parameter that they are not going to be null(!)
+                        title: titleRef.current!.value,
+                        markdown: markdownRef.current!.value,
+                        tagCreateDtos: selectedTags
+                    });
+                    console.log("Autosave");
+                }
+            }, 300000)
+            return () => clearTimeout(timerId);
+        }
+    });
+
+    function onCancelClick() {
+
+    }
 
     function handleSubmit(e: FormEvent) {
         e.preventDefault();
@@ -73,7 +97,7 @@ export function NoteForm({ onSubmit, availableTags, title = "", markdown = "", t
                 <Stack direction="horizontal" gap={2} className="justify-content-end">
                     <Button type="submit" variant="primary">Save</Button>
                     <Link to="..">
-                        <Button type="button" variant="outline-secondary">Cancel</Button>
+                        <Button onClick={onCancelClick} type="button" variant="outline-secondary">Cancel</Button>
                     </Link>
                 </Stack>
             </Stack>
